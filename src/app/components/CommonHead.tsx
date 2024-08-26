cd8e96b56c5c3f02f9b75834d305963a33c9d8b4
baltazarottonello <baltazarottonello@gmail.com>
2024-08-26 01:01:08 -0300

    deleted readme

---
+++ src/app/components/CommonHead.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import "../globals.css";
+
+export default function CommonHead() {
+  return (
+    <head>
+      <title>bookRepository</title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+      />
+    </head>
+  );
+}
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,19 @@
+import "../../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="socials-container">
+        <a href="#">
+          <img src="instagram.svg" />
+        </a>
+        <a href="#">
+          <img src="github.svg" />
+        </a>
+        <a href="#">
+          <img src="linkedin.svg" />
+        </a>
+      </div>
+    </footer>
+  );
+}
